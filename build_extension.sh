d0d14eaec682a05c9249faf625951fe9965452dc
TheNuclearNexus <39636175+TheNuclearNexus@users.noreply.github.com>
2024-11-13 13:47:49 -0500

    fix: use pip install to get all dependencies

--- build_extension.sh
+++ build_extension.sh
@@ -1,14 +1,13 @@
 
 rm -rf build/*
 
-mkdir build
-mkdir build/language_server
+# mkdir build/language_server
 
-cp -r language_server/* build/language_server
+# cp -r language_server/* build/language_server
 
+
+pip install . --target build
 echo "from language_server.__main__ import main; main()" > build/__main__.py
-
-pip install ../mecha ../bolt --target build
 
 rm -rf build/numpy*
 
