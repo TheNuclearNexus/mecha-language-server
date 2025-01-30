2397053db6d0516226eaa2deaa495e7446d184a6
TheNuclearNexus <39636175+TheNuclearNexus@users.noreply.github.com>
2025-01-30 14:09:04 -0500

    chore: semantic release

--- extension/clean_zipapp.js
+++ extension/clean_zipapp.js
@@ -5,28 +5,35 @@ const packagesToRemove = [
     "numpy",
     "pip",
     "setuptools",
-    "pydantic_core",
-    "_pytest"
-]
+    "pydantic",
+    "_pytest",
+    "jinja2"
+];
 
 async function main() {
     const zip = await JsZip.loadAsync(fs.readFileSync("language_server.pyz"));
 
-    const paths = []
+    const paths = [];
     zip.forEach((path, file) => {
         for (const package of packagesToRemove) {
-            if (path.startsWith("site-packages/" + package)) {
-                paths.push(path)
+            if (path.startsWith(package)) {
+                paths.push(path);
+                return;
             }
         }
+        if (path.includes("__pycache__"))
+            paths.push(path)
     });
 
     for (const path of paths) {
-        console.log('removed', path)
-        zip.remove(path)
+        console.log("removed", path);
+        zip.remove(path);
     }
 
-    fs.writeFileSync("language_server.pyz", await zip.generateAsync({type: "nodebuffer"}))
+    fs.writeFileSync(
+        "language_server.pyz",
+        await zip.generateAsync({ type: "nodebuffer" })
+    );
 }
 
-main()
+main();
