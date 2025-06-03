6dabfca3b2db3c5b077957f838e7e599d78ebfd8
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2025-06-04 00:20:43 +0200

    chore: remove debug logs

--- aegis-jetbrains/src/main/kotlin/com/github/thenuclearnexus/aegis/ide/run/BeetRunConfigurationUtil.kt
+++ aegis-jetbrains/src/main/kotlin/com/github/thenuclearnexus/aegis/ide/run/BeetRunConfigurationUtil.kt
@@ -3,14 +3,9 @@ package com.github.thenuclearnexus.aegis.ide.run
 import com.github.thenuclearnexus.aegis.hasBeetFile
 import com.intellij.execution.RunManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.diagnostic.Logger
 
 fun addBeetRunConfigurationsIfNeeded(project: Project) {
-    val logger = Logger.getInstance("BeetRunConfigurationUtil")
-    if (!hasBeetFile(project)) {
-        logger.info("No beet file found in project: ${project.name}")
-        return
-    }
+    if (!hasBeetFile(project)) return
     val runManager = RunManager.getInstance(project)
     val existingNames = runManager.allConfigurationsList.map { it.name }
 
@@ -21,16 +16,10 @@ fun addBeetRunConfigurationsIfNeeded(project: Project) {
     if ("Beet Build" !in existingNames) {
         val buildSettings = runManager.createConfiguration("Beet Build", buildFactory)
         runManager.addConfiguration(buildSettings)
-        logger.info("Added Beet Build run configuration to project: ${project.name}")
-    } else {
-        logger.info("Beet Build run configuration already exists in project: ${project.name}")
     }
     if ("Beet Watch" !in existingNames) {
         val watchSettings = runManager.createConfiguration("Beet Watch", watchFactory)
         runManager.addConfiguration(watchSettings)
-        logger.info("Added Beet Watch run configuration to project: ${project.name}")
-    } else {
-        logger.info("Beet Watch run configuration already exists in project: ${project.name}")
     }
 }
 
